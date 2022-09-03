65ba4e27edc5388989172f83228d84d7401d2c7f
Jack Bird <jackbird050@gmail.com>
2022-09-03 20:16:42 +0100

    first implementation of all end points needed

--- truelayer.py
+++ truelayer.py
@@ -1,31 +1,34 @@
-import sqlite3
 import requests
 import time
 import os
-import json
+
 from dotenv import load_dotenv
 
-load_dotenv()
-
 class TrueLayerHandler:
-    def __init__(self, IP):
+    def __init__(self, client_id, client_secret, redirect_uri, IP, refresh_token=None):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.redirect_uri = redirect_uri
         self.ip = IP
 
         self.access_token = None
-        self.refresh_token = None
-
-
-        self.client_id = os.getenv('TRUELAYER_CLIENT_ID')
-        self.client_secret = os.getenv('TRUELAYER_CLIENT_SECRET')
-        self.redirect_uri = os.getenv('TRUELAYER_REDIRECT_URI')
+        self.refresh_token = refresh_token
 
         self.base_url = "https://api.truelayer.com/"
         self.urls = {
             'token': "https://auth.truelayer.com/connect/token",
-            'accounts': 'data/v1/accounts'
+            'accounts': 'data/v1/accounts',
+            'batchTransactions': 'data/v1/batch/transactions'
             }
 
         self.urls['transactions'] = lambda account_id : f'{self.urls["accounts"]}/{account_id}/transactions'
+        self.urls['pending'] = lambda account_id : f'{self.urls["accounts"]}/{account_id}/transactions/pending'
+        self.urls['balance'] = lambda account_id : f'{self.urls["accounts"]}/{account_id}/balance'
+        self.urls['standing_orders'] = lambda account_id : f'{self.urls["accounts"]}/{account_id}/standing_orders'
+        self.urls['direct_debits'] = lambda account_id : f'{self.urls["accounts"]}/{account_id}/direct_debits'
+
+        if refresh_token:
+            self.refreshAccessToken()
 
     def authLinkBuilder(self):
         return "https://auth.truelayer.com/?response_type=code&client_id=jackbird-64e668&scope=info%20accounts%20balance%20cards%20transactions%20direct_debits%20standing_orders%20offline_access&redirect_uri=https://console.truelayer.com/redirect-page&providers=uk-ob-all%20uk-oauth-all"
@@ -52,6 +55,8 @@ class TrueLayerHandler:
             self.access_token = response['access_token']
             self.refresh_token = response['refresh_token']
 
+        return response
+
     def refreshAccessToken(self):
         url = self.urls['token']
 
@@ -72,6 +77,9 @@ class TrueLayerHandler:
             response = response.json()
             self.access_token = response['access_token']
             self.refresh_token = response['refresh_token']
+        
+        return response
+
 
     def requestAccounts(self):
         url = f"{self.base_url}{self.urls['accounts']}"
@@ -99,6 +107,61 @@ class TrueLayerHandler:
 
         return requests.get(url, headers=headers)
 
+    def requestBalance(self, account_id):
+        url = f"{self.base_url}{self.urls['balance'](account_id)}"
+
+        headers = {
+            "Accept": "application/json",
+            "X-Client-Correlation-Id": f"{self.client_id}",
+            "X-PSU-IP": f"{self.ip}",
+            "Authorization": f"Bearer {self.access_token}"
+        }
+
+        return requests.get(url, headers=headers)
+
+    def requestStandingOrders(self, account_id):
+        url = f"{self.base_url}{self.urls['standing_orders'](account_id)}"
+
+        headers = {
+            "Accept": "application/json",
+            "X-Client-Correlation-Id": f"{self.client_id}",
+            "X-PSU-IP": f"{self.ip}",
+            "Authorization": f"Bearer {self.access_token}"
+        }
+
+        return requests.get(url, headers=headers)
+
+    def requestDirectDebits(self, account_id):
+        url = f"{self.base_url}{self.urls['direct_debits'](account_id)}"
+
+        headers = {
+            "Accept": "application/json",
+            "X-Client-Correlation-Id": f"{self.client_id}",
+            "X-PSU-IP": f"{self.ip}",
+            "Authorization": f"Bearer {self.access_token}"
+        }
+
+        return requests.get(url, headers=headers)
+
+    def requestBatchCall(self, date_from, date_to, pending=True, balance=False):
+        url = f"{self.base_url}{self.urls['batchTransactions']}"
+
+        payload = {
+            "pending": pending,
+            "balance": balance,
+            "from": date_from,
+            "to": date_to
+        }
+        headers = {
+            "Accept": "application/json",
+            "X-Client-Correlation-Id": f"{self.client_id}",
+            "X-PSU-IP": f"{self.ip}",
+            "Authorization": f"Bearer {self.access_token}"
+        }
+
+        return requests.post(url, json=payload, headers=headers)
+
+
     def requestResults(self, results_url):
         time.sleep(1)
         headers = {
@@ -108,8 +171,6 @@ class TrueLayerHandler:
         return requests.get(results_url, headers=headers)
 
     def makeRequest(self, request, *args, called=1, **kwargs):
-        print('called', called)
-        print('args', args)
         called += 1
         if called > 3: 
             return "Too many errors"
@@ -127,4 +188,3 @@ class TrueLayerHandler:
             return self.makeRequest(request, called=called, *args, **kwargs)
 
         return response
-
