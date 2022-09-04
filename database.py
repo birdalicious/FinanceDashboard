811685c95215d45e1a61ae77787f1e4f6c08b1fb
Jack Bird <jackbird050@gmail.com>
2022-09-05 00:51:02 +0100

    Added basic insert for transactions

--- database.py
+++ database.py
@@ -2,20 +2,19 @@ import sqlite3
 
 class DatabaseHandler:
     def __init__(self, file):
-        con = sqlite3.Connection(file)
-        cursor = con.cursor()
+        self.con = sqlite3.Connection(file)
+        self.cursor = self.con.cursor()
 
-        cursor.execute(
+        self.cursor.execute(
             ''' 
             CREATE TABLE IF NOT EXISTS linked_accounts (
-                id INT AUTO_INCREMENT,
-                refresh_token VARCHAR(64),
-                PRIMARY_KEY id
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                refresh_token VARCHAR(64)
             );
             '''
         )
 
-        cursor.execute(
+        self.cursor.execute(
             '''
             CREATE TABLE IF NOT EXISTS `accounts` (
                 `account_id` VARCHAR(32) NOT NULL,
@@ -31,13 +30,14 @@ class DatabaseHandler:
             '''
         )
 
-        cursor.execute(
+        self.cursor.execute(
             '''
             CREATE TABLE IF NOT EXISTS `transactions` (
-                `id` INT AUTO_INCREMENT,
+                `id` INTEGER PRIMARY KEY AUTOINCREMENT,
                 `account_id` VARCHAR(32) NOT NULL,
                 `timestamp` DATE NOT NULL,
                 `amount` DECIMAL NOT NULL,
+                `currency` VARCHAR(4) NOT NULL,
                 `merchant_name` VARCHAR(128),
                 `description` TEXT,
                 `type` VARCHAR(10) NOT NULL,
@@ -45,10 +45,61 @@ class DatabaseHandler:
                 `classification` TEXT,
                 `balance_amount` DECIMAL NOT NULL,
                 `balance_currency` VARCHAR(4) NOT NULL,
-                PRIMARY KEY (`id`),
                 FOREIGN KEY (account_id) REFERENCES accounts(account_id)
             );
             '''
         )
 
-        con.commit()
+        self.con.commit()
+
+    def insertTransaction(self, **kwargs):
+        balance_amount = kwargs['running_balance']['amount']
+        balance_currency = kwargs['running_balance']['currency']
+
+        inserts = (
+            kwargs['account_id'],
+            kwargs['timestamp'][:10],
+            kwargs['amount'],
+            kwargs['currency'],
+            kwargs.get('merchant_name', None),
+            kwargs['description'],
+            kwargs['transaction_type'],
+            kwargs['transaction_category'],
+            str(kwargs['transaction_classification']),
+            balance_amount,
+            balance_currency
+            )
+
+        self.cursor.execute(
+            '''
+            INSERT INTO transactions (
+                account_id,
+                timestamp,
+                amount,
+                currency,
+                merchant_name,
+                description,
+                type,
+                category,
+                classification,
+                balance_amount,
+                balance_currency
+            )
+            VALUES (
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?
+            )
+            ''',
+            inserts
+        )
+
+        self.con.commit()
