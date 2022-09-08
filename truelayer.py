f5d19b157064492954fc7b94ccc68e11fc48f973
Jack Bird <jackbird050@gmail.com>
2022-09-08 15:52:39 +0100

    Started rewrite using decorators and consideration for card endpoints

--- truelayer.py
+++ truelayer.py
@@ -1,6 +1,27 @@
 import requests
 import time
 
+def tlRequest(request):
+    def inner(self, *args, called=1 **kwargs):
+        called += 1
+        if called > 3:
+            return "Too many errors"
+
+        response = request(*args, **kwargs)
+
+        if response.status_code == 202:
+            if 'results_uri' in response.json():
+                url = response.json()['result_uri']
+                return self.getResults(url)
+            return response
+        
+        if response.status_code == 401:
+            self.refreshAccessToken()
+            return request(called=called, *args, **kwargs)
+
+    return inner
+
+
 class TrueLayerHandler:
     def __init__(self, client_id, client_secret, redirect_uri, IP, refresh_token=None):
         self.client_id = client_id
@@ -11,27 +32,41 @@ class TrueLayerHandler:
         self.access_token = None
         self.refresh_token = refresh_token
 
-        self.base_url = "https://api.truelayer.com/"
-        self.urls = {
-            'token': "https://auth.truelayer.com/connect/token",
-            'accounts': 'data/v1/accounts',
-            'batchTransactions': 'data/v1/batch/transactions'
-            }
-
-        self.urls['transactions'] = lambda account_id : f'{self.urls["accounts"]}/{account_id}/transactions'
-        self.urls['pending'] = lambda account_id : f'{self.urls["accounts"]}/{account_id}/transactions/pending'
-        self.urls['balance'] = lambda account_id : f'{self.urls["accounts"]}/{account_id}/balance'
-        self.urls['standing_orders'] = lambda account_id : f'{self.urls["accounts"]}/{account_id}/standing_orders'
-        self.urls['direct_debits'] = lambda account_id : f'{self.urls["accounts"]}/{account_id}/direct_debits'
-
-        if refresh_token:
+        if self.refresh_token:
             self.refreshAccessToken()
 
-    def authLinkBuilder(self):
-        return "https://auth.truelayer.com/?response_type=code&client_id=jackbird-64e668&scope=info%20accounts%20balance%20cards%20transactions%20direct_debits%20standing_orders%20offline_access&redirect_uri=https://console.truelayer.com/redirect-page&providers=uk-ob-all%20uk-oauth-all"
 
+    def endpoint(self, type, key=None):
+        base_url = "https://api.truelayer.com"
+
+        types = {
+            'accounts': 'data/v1/accounts',
+            'cards': 'data/v1/cards',
+            'auth': 'https://auth.truelayer.com/connect/token',
+        }
+
+        endpoints = {
+            'transactions': lambda account_id: f"{account_id}/transactions",
+            'pending': lambda account_id: f"{account_id}/transactions/pending",
+            'balance': lambda account_id: f"{account_id}/balance",
+            'standing_orders': lambda account_id: f"{account_id}/standing_orders",
+            'direct_debits': lambda account_id: f"{account_id}/direct_debits",
+        }
+
+        if type == 'auth':
+            return types['auth']
+
+        url = f"{base_url}/{types[type]}"
+
+        if key:
+            return lambda x : f"{url}/{endpoints[key](x)}"
+
+        return url
+
+
+    @tlRequest
     def authSetup(self, exCode):
-        url = self.urls['token']
+        url = self.endpoint('auth')
 
         payload = {
             "grant_type": "authorization_code",
@@ -54,8 +89,9 @@ class TrueLayerHandler:
 
         return response
 
+    @tlRequest
     def refreshAccessToken(self):
-        url = self.urls['token']
+        url = self.endpoint('auth')
 
         payload = {
             "grant_type": "refresh_token",
@@ -74,13 +110,11 @@ class TrueLayerHandler:
             res = response.json()
             self.access_token = res['access_token']
             self.refresh_token = res['refresh_token']
-        
+
         return response
 
 
-    def requestAccounts(self):
-        url = f"{self.base_url}{self.urls['accounts']}"
-
+    def baseGetRequest(self, url):
         headers = {
             "Accept": "application/json",
             "X-Client-Correlation-Id": self.client_id,
@@ -90,113 +124,25 @@ class TrueLayerHandler:
 
         return requests.get(url, headers=headers)
 
-    def requestTransactions(self, account_id, date_from=None, date_to=None):
-        url = f"{self.base_url}{self.urls['transactions'](account_id)}"
-        if date_from and date_to:
-            url += f"?from={date_from}&to={date_to}"
-
-        headers = {
-            "Accept": "application/json",
-            "X-Client-Correlation-Id": self.client_id,
-            "X-PSU-IP": self.ip,
-            "Authorization": f"Bearer {self.access_token}"
-        }
-
-        return requests.get(url, headers=headers)
-    
-    def requestPendingTransactions(self, account_id, date_from=None, date_to=None):
-        url = f"{self.base_url}{self.urls['pending'](account_id)}"
-        if date_from and date_to:
-            url += f"?from={date_from}&to={date_to}"
-
-        headers = {
-            "Accept": "application/json",
-            "X-Client-Correlation-Id": self.client_id,
-            "X-PSU-IP": self.ip,
-            "Authorization": f"Bearer {self.access_token}"
-        }
-
-        return requests.get(url, headers=headers)
-
-    def requestBalance(self, account_id):
-        url = f"{self.base_url}{self.urls['balance'](account_id)}"
-
-        headers = {
-            "Accept": "application/json",
-            "X-Client-Correlation-Id": self.client_id,
-            "X-PSU-IP": self.ip,
-            "Authorization": f"Bearer {self.access_token}"
-        }
-
-        return requests.get(url, headers=headers)
-
-    def requestStandingOrders(self, account_id):
-        url = f"{self.base_url}{self.urls['standing_orders'](account_id)}"
-
-        headers = {
-            "Accept": "application/json",
-            "X-Client-Correlation-Id": self.client_id,
-            "X-PSU-IP": self.ip,
-            "Authorization": f"Bearer {self.access_token}"
-        }
-
-        return requests.get(url, headers=headers)
-
-    def requestDirectDebits(self, account_id):
-        url = f"{self.base_url}{self.urls['direct_debits'](account_id)}"
-
-        headers = {
-            "Accept": "application/json",
-            "X-Client-Correlation-Id": self.client_id,
-            "X-PSU-IP": self.ip,
-            "Authorization": f"Bearer {self.access_token}"
-        }
-
-        return requests.get(url, headers=headers)
-
-    def requestBatchCall(self, date_from, date_to, pending=True, balance=False):
-        url = f"{self.base_url}{self.urls['batchTransactions']}"
-
-        payload = {
-            "pending": pending,
-            "balance": balance,
-            "from": date_from,
-            "to": date_to
-        }
-        headers = {
-            "Accept": "application/json",
-            "X-Client-Correlation-Id": self.client_id,
-            "X-PSU-IP": self.ip,
-            "Authorization": f"Bearer {self.access_token}"
-        }
-
-        return requests.post(url, json=payload, headers=headers)
-
-
-    def requestResults(self, results_url):
+    @tlRequest
+    def getResults(self, url):
         time.sleep(1)
-        headers = {
-            "Authorization": f"Bearer {self.access_token}",
-            "Accept": "application/json"
-            }
-        return requests.get(results_url, headers=headers)
+        return self.baseGetRequest(url)
 
-    def makeRequest(self, request, *args, called=1, **kwargs):
-        called += 1
-        if called > 3: 
-            return "Too many errors"
+    @tlRequest
+    def getAccounts(self):
+        url = self.endpoint('accounts')
+        return self.baseGetRequest(url)
+    
+    @tlRequest
+    def getCards(self):
+        url = self.endpoint('cards')
+        return self.baseGetRequest(url)
 
-        response = request(*args, **kwargs)
-
-        if response.status_code == 202:
-            if 'results_uri' in response.json():
-                redirect = response.json()['results_uri']
-                return self.makeRequest(self.requestResults, redirect)
-            return response
-
-        if response.status_code == 401:
-            print(response.text)
-            self.refreshAccessToken()
-            return self.makeRequest(request, called=called, *args, **kwargs)
-
-        return response
+    @tlRequest
+    def getBalance(self, account_id, card=False):
+        if card:
+            url = self.endpoint('cards', 'balance')(account_id)
+        else:
+            url = self.endpoint('accounts', 'balance')(account_id)
+        return self.baseGetRequest(url)
