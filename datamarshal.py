2afa2618357d7c69a3c2399de5017b476d5dbe5c
Jack Bird <jackbird050@gmail.com>
2022-09-11 13:35:56 +0100

    Start for pulling transactions

--- datamarshal.py
+++ datamarshal.py
@@ -1,4 +1,3 @@
-from distutils.errors import LinkError
 import os
 import requests
 from datetime import date, datetime, timedelta
@@ -141,3 +140,54 @@ class DataMarshaller:
         for card in cards:
             self.db.addCard(link_id=link_id, **card)
             self.refreshOverdraft(card['account_id'], link_id, cards=True)
+
+    def pullLinkTransactions(self, link_id, date_from, date_to):
+        tlHandler = self.tlHandlers[link_id]
+
+        response = tlHandler.getTransactions(date_from, date_to)
+        if response.json()['status'] == 'Failed':
+            print(response.json())
+            return
+
+        for accountCard in ["accounts", "cards"]:
+            for account in response.json()['results'].get(accountCard, []):
+                account_id = account['account_id']
+                transactions = account['transactions']
+                
+                lastTransaction = self.db.getLastTransaction(account_id)
+                overlap_date_to = date_to if not lastTransaction else lastTransaction['timestamp']
+                overlap_date_from = datetime.strptime(date_from, '%Y-%m-%d').date() - timedelta(days=1)
+
+                overlap = {o[1] for o in self.db.getTransactions(account_id, overlap_date_from, overlap_date_to)}
+
+                transactions = [r for r in transactions[::-1] if r.get('normalised_provider_transaction_id', r['transaction_id']) not in overlap]
+                if len(transactions) == 0:
+                    return 
+
+                if account['balance']['current'] == transactions[-1].get("running_balance", {"amount": None})["amount"]:
+                    for transaction in transactions:
+                        self.db.insertTransaction(account_id=account_id, **transaction)
+                    return
+
+                buffer = []
+                currentDate = transactions[0]['timestamp']
+
+                # Insert transactions flipping the results of each day for a contiguous ordering
+                for transaction in transactions:
+                    if transaction['timestamp'] == currentDate:
+                        buffer.append(transaction)
+                    else:
+                        for t in buffer[::-1]:
+                            self.db.insertTransaction(account_id=account_id, **t)
+                        
+                        buffer = [transaction]
+                        currentDate = transaction['timestamp']
+                for t in buffer[::-1]:
+                    self.db.insertTransaction(
+                        account_id=account_id,
+                        running_balance={
+                            'amount': account['balance']['current'],
+                            'currency': account['balance']['currency']
+                            },
+                        **t
+                    )
